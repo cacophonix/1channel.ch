d720ac4a0691b11dad2e36b5775c298178701cca
cacophonix <ashiqulmostofa@ymail.com>
2012-08-10 04:50:04 +0600

    series name series link done

--- find_series_in_page.py
+++ find_series_in_page.py
@@ -3,25 +3,33 @@ import opener
 import re
 
 
+
+
+def i_have_gotten_series_name(url,name):
+	pass
+	
+
+
+
+
 def i_have_gotten_page_number(url):
 	
 	data=opener.fetch(url)['data']
 	soup=BeautifulSoup(data)
-	
 	l=soup.find_all('a')
-	
-	reg=re.compile(r'.*/watch.*')
-	
+	reg=re.compile(r'.*/watch-\d+-(.*)')
 	for i in l:
-		
 		if not i.has_key('href'):
 			continue;
-			
+		if not i.has_key('title'):
+			continue;
 		link =i.get('href')
-		
 		m=reg.match(link)
 		if m:
-			print m.group(0)
+			series_name=i.get('title')
+			series_link="http://www.1channel.ch"+m.group(0)
+			print series_name,series_link
+			#~ got series name and series link
 
 
 def get_page_count_and_go_deeper(url):
@@ -37,8 +45,8 @@ def get_page_count_and_go_deeper(url):
 	
 	for i in range(1,page_count+1):
 		new_url=url+"&page="+str(i)
+		#~ i have got the new url and now i have to go through all the urls
 		print new_url
-		
 	
 
 
@@ -51,6 +59,7 @@ def tester(url):
 
 if __name__=='__main__':
 	
-	i_have_gotten_page_number('http://www.1channel.ch/?letter=123&page=1')
+	#~ i_have_gotten_page_number('http://www.1channel.ch/?letter=123&tv&page=1')
+	get_page_count_and_go_deeper('http://www.1channel.ch/?letter=123&tv')
 	
 	
